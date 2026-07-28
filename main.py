1f6faf127df259f12381abbb417cbdea23d13b7d
ayushdev402-prog <ayushdev402@gmail.com>
2026-07-28 20:02:14 +0530

    Update CORS policy to allow all production origins

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origin_regex=r"https?://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
